e3ccf1a3a59812ff0538f2f5f0bd9320975e7e8f
ayoubbelmaati <101731234+ayoubbelmaati@users.noreply.github.com>
2023-04-07 23:29:58 +0000

    INC-app-1: project nuxt vue 3 tailwindcss

---
+++ incidentappmonitor/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world! 
+  </h1>
+  <button class="btntest">
+    Button ayoub
+  </button>
+  <button class="btncancel">
+    Button lamiae
+  </button>
+</template>
