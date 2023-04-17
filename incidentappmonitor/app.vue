dd3840cbaacac2ddc31266db26bc6ff96e79b4db
ayoubbelmaati <101731234+ayoubbelmaati@users.noreply.github.com>
2023-04-17 15:39:34 +0000

    first test commit

--- incidentappmonitor/app.vue
+++ incidentappmonitor/app.vue
@@ -1,11 +1,11 @@
 <template>
- 
-<div>
-  <NuxtLayout>
-    <NuxtPage/>
-  </NuxtLayout>
-</div>
-
-
-
+  <h1 class="text-3xl font-bold underline">
+    Hello world! 
+  </h1>
+  <button class="btntest">
+    Button ayoub
+  </button>
+  <button class="btncancel">
+    Button lamiae
+  </button>
 </template>
